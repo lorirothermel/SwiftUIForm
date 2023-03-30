90ba8cab14c78363facb310374e92f8047ca3b40
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-30 03:24:18 -0400

    Initial Commit

---
+++ SwiftUIForm/SwiftUIFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormApp.swift
+//  SwiftUIForm
+//
+//  Created by Lori Rothermel on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
